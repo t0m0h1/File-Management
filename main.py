adc3564bca6f450e065e569e8a99830fb0ac3757
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2023-12-11 01:17:42 +0000

    copy, move, delete

---
+++ main.py
@@ -0,0 +1,28 @@
+import shutil, os
+
+
+def main():
+    # copy a file
+    shutil.copy('data.txt', 'data_copy.txt')
+
+    # copy a file and rename it
+    shutil.copy('data.txt', 'data_copy2.txt')
+
+    # copy a directory
+    shutil.copytree('data', 'data_copy')
+
+    # move a file
+    shutil.move('data.txt', 'data_copy/data.txt')
+
+    # move a directory
+    shutil.move('data', 'data_copy/data')
+
+    # delete a file
+    shutil.rmtree('data_copy')
+    os.remove('data_copy.txt')
+
+    # delete a directory
+    os.rmdir('data_copy')
+
+if  __name__ == '__main__':
+    main()
